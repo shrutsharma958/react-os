0d5a207178f8dde6b0fc15ff3aa10912e581759d
Shrut Sharma <shrutsharma958@gmail.com>
2026-01-24 14:28:23 +0530

    Added option of available currencies and default currency

--- cur/src/App.jsx
+++ cur/src/App.jsx
@@ -3,22 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import InputBox from './components/InputBox'
+import Currency from './hooks/Currency'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[from,setFrom]=useState("usd")
+  const[to,setTo]=useState("inr")
+  const currency=Currency(from)
+  const currencyOptions = Object.keys(currency)
 
   return (
    <div style={{backgroundColor:"#8F00FF "}} className='w-full h-screen flex'>
     <div className=' px-4 py-4 max-w-lg flex rounded-2xl mx-auto my-auto backdrop-blur-md bg-white/30 w-full justify-center flex-wrap'>
     <div className='w-full bg-amber-50 rounded-2xl px-4 py-4 relative flex justify-center'>
-       <InputBox/>
+       <InputBox currencys={currency}label={from}/>
       <button className='absolute left-1/2 -translate-x-1/2 -bottom-5 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5' style={{backgroundColor:"#6495ED"}}>
         Swap
       </button>
     </div>
     
     <div  className='w-full bg-amber-50 rounded-2xl px-4 py-4 mt-2 flex justify-center'>
-       <InputBox/>
+       <InputBox currencys={currency} label={to}/>
     </div>
     <button className='rounded-2xl py-3 mt-5  text-amber-50 w-full text-2xl' style={{backgroundColor:'#6495ED'}}>Convert</button>
     </div>
--- cur/src/components/InputBox.jsx
+++ cur/src/components/InputBox.jsx
@@ -1,27 +1,30 @@
-import React, {useId} from 'react'
 
-function InputBox({
-}) {
+
+
+function InputBox({label,currencys}) {
+
+    const currencyOptions =  Object.keys(currencys) ;
+    
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex`}>
             <div className="w-1/2">
-                <label   className="text-black/40 mb-2 inline-block">
-                    label
+                <label   className=" mb-2 inline-block text-2xl text-black">
+                    {label}
                 </label>
                 <input
-                    className="outline-none w-full bg-transparent py-1.5"
+                    className="shadow-2xl border px-2 w-full bg-transparent py-2"
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <p className="text-black mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                 >
                        
-                            <option >
-                                usd
-                            </option>
-                        
+                           
+                           {currencyOptions.map((currency)=>{
+                           return <option value={currency}>{currency}</option>
+})}
                 
                 </select>
             </div>
